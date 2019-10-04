6fb781de32af0802553b0890e4b2b11d9a4d57aa
Michael Johnson <cbmmd42@yahoo.com>
2019-10-03 22:19:46 -0700

    Added include IPlayer.h

--- UnitTests/IPlayerTests/IPlayerTests.cpp
+++ UnitTests/IPlayerTests/IPlayerTests.cpp
@@ -1,11 +1,13 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#include "IPlayer.h"
 
 using ::testing::Return;
 using ::testing::Assign;
 
 
 TEST(IPlayerTests, Player){
+	
 	EXPECT_TRUE(true);	
 }
 
