2c413d993156b869e6a4bb09b9f37a8ee6908db8
Michael Johnson <cbmmd42@yahoo.com>
2019-10-03 18:48:55 -0700

    Added isAlive method to IPlayer

--- headers/IPlayer.h
+++ headers/IPlayer.h
@@ -7,6 +7,8 @@ class IPlayer {
 	virtual std::string move(int roomChoice);
 	virtual bool shoot(int roomNo1,int roomNo2, int roomNo3) = 0;
 	virtual std::string soundsHeard();
+	virtual bool isAlive() = 0;
+	virtual bool wampusLives() = 0;
 };
 
 #endif /*IPLAYER_HPP*/
