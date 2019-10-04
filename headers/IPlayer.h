3e4d80bb63e4479556adf2773bf40419d04c22a4
Michael Johnson <cbmmd42@yahoo.com>
2019-10-03 22:20:29 -0700

    Removed pure virtual functions

--- headers/IPlayer.h
+++ headers/IPlayer.h
@@ -5,10 +5,10 @@
 class IPlayer {
 	public:
 	virtual std::string move(int roomChoice);
-	virtual bool shoot(int roomNo1,int roomNo2, int roomNo3) = 0;
+	virtual bool shoot(int roomNo1,int roomNo2, int roomNo3);
 	virtual std::string soundsHeard();
-	virtual bool isAlive() = 0;
-	virtual bool wampusLives() = 0;
+	virtual bool isAlive();
+	virtual bool wampusLives();
 };
 
 #endif /*IPLAYER_HPP*/
