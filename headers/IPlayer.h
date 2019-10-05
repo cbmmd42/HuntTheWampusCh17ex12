09454b61561c7b004c7c4d788380d6e87bb9999f
Michael Johnson <cbmmd42@yahoo.com>
2019-10-04 20:53:50 -0700

    Added ILocation

--- headers/IPlayer.h
+++ headers/IPlayer.h
@@ -1,14 +1,19 @@
 #ifndef IPLAYER_HPP
 #define IPLAYER_HPP
 #include <iostream>
+#include "ILocation.h"
 
 class IPlayer {
-	public:
+public:
+	IPlayer();
+	IPlayer(ILocation& l);
 	virtual std::string move(int roomChoice);
 	virtual bool shoot(int roomNo1,int roomNo2, int roomNo3);
 	virtual std::string soundsHeard();
 	virtual bool isAlive();
 	virtual bool wampusLives();
+private:
+	ILocation* playerLocation;	
 };
 
 #endif /*IPLAYER_HPP*/
