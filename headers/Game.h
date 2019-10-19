9f54dd3f29962bbb531d24573ae4c35db5fc4e0b
cbmmd42@yahoo.com <cbmmd42@yahoo.com>
2019-10-18 21:55:25 -0700

    changed IPlayerTests to playertests and modified folder

--- headers/Game.h
+++ headers/Game.h
@@ -3,21 +3,22 @@
 #include <iostream>
 #include "IPlayer.h"
 #include "IUserInput.h"
+#include "IGame.h"
 
-class Game {
+class Game : public IGame {
 	IPlayer* player;
 	IUserInput* gameInput;
 public:
 	Game(IPlayer& p, IUserInput& i);
-	virtual void prompt(std::string s);
-	virtual std::string connectedRooms(int& first, int& second, int& third);
+	virtual void prompt(std::string s) override;
+	virtual std::string connectedRooms(int& first, int& second, int& third) override;
 	virtual std::string move(int roomChoice);
-	virtual std::string shoot(int roomNo1, int roomNo2, int roomNo3);
-	virtual std::string indication();
-	virtual char userInput();
-	virtual void userInput(int& roomNumber);
-	virtual void userInput(int& firstRoom, int& secondRoom, int& thirdRoom);
-	virtual bool verify();
+	virtual std::string shoot(int roomNo1, int roomNo2, int roomNo3) override;
+	virtual std::string indication() override;
+	virtual char userInput() override;
+	virtual void userInput(int& roomNumber) override;
+	virtual void userInput(int& firstRoom, int& secondRoom, int& thirdRoom) override;
+	virtual bool verify() override;
 };
 
 #endif /*IGAME_HPP*/
