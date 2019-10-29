2278023e411d2d7de30d84edeac2bef3f1334688
cbmmd42@yahoo.com <cbmmd42@yahoo.com>
2019-10-28 22:56:24 -0700

    Added Unit Tests File

---
+++ UnitTests/UserInputTests/UserInputTests.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "UserInput.h"
+
+using ::testing::Return;
+using ::testing::Assign;
+using ::testing::_;
+
+TEST(UserInputTests, UserInput){
+
+}
